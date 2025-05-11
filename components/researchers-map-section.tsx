cac78251ce7dd9ac83588190e36d2f6c33aa6770
Quantfun <quantfun975@gmail.com>
2025-05-11 21:31:07 +0700

    a

--- components/researchers-map-section.tsx
+++ components/researchers-map-section.tsx
@@ -9,70 +9,70 @@ const researcherLocations = [
     id: 1,
     name: "Dana-Farber Cancer Institute",
     country: "USA",
-    coordinates: [42.3376, -71.1037],
+    coordinates: [42.3376, -71.1037] as [number, number],
     countryCode: "US"
   },
   {
     id: 2,
     name: "MD Anderson Cancer Center",
     country: "USA",
-    coordinates: [29.7604, -95.3698],
+    coordinates: [29.7604, -95.3698] as [number, number],
     countryCode: "US"
   },
   {
     id: 3,
     name: "National Cancer Institute – NIH",
     country: "USA",
-    coordinates: [39.0029, -77.1043],
+    coordinates: [39.0029, -77.1043] as [number, number],
     countryCode: "US"
   },
   {
     id: 4,
     name: "Memorial Sloan Kettering Cancer Center",
     country: "USA",
-    coordinates: [40.7645, -73.9565],
+    coordinates: [40.7645, -73.9565] as [number, number],
     countryCode: "US"
   },
   {
     id: 5,
     name: "Cancer Research UK",
     country: "United Kingdom",
-    coordinates: [51.5259, -0.1289],
+    coordinates: [51.5259, -0.1289] as [number, number],
     countryCode: "GB"
   },
   {
     id: 6,
     name: "German Cancer Research Center – DKFZ",
     country: "Germany",
-    coordinates: [49.4142, 8.6750],
+    coordinates: [49.4142, 8.6750] as [number, number],
     countryCode: "DE"
   },
   {
     id: 7,
     name: "Institut Curie",
     country: "France",
-    coordinates: [48.8453, 2.3434],
+    coordinates: [48.8453, 2.3434] as [number, number],
     countryCode: "FR"
   },
   {
     id: 8,
     name: "Peter MacCallum Cancer Centre",
     country: "Australia",
-    coordinates: [-37.8136, 144.9631],
+    coordinates: [-37.8136, 144.9631] as [number, number],
     countryCode: "AU"
   },
   {
     id: 9,
     name: "RIKEN Center for Integrative Medical Sciences",
     country: "Japan",
-    coordinates: [35.2288, 139.1027],
+    coordinates: [35.2288, 139.1027] as [number, number],
     countryCode: "JP"
   },
   {
     id: 10,
     name: "A*STAR Institute of Molecular and Cell Biology",
     country: "Singapore",
-    coordinates: [1.2956, 103.7877],
+    coordinates: [1.2956, 103.7877] as [number, number],
     countryCode: "SG"
   }
 ];
